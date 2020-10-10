6b45607149dc34f0122aaf56c5b1e20c854f14b4
Paola351 <paola.rodilosso@gmail.com>
2020-10-10 17:01:59 +0200

    added loggerfile

--- checkTasks.py
+++ checkTasks.py
@@ -1,116 +1,121 @@
 import configparser, sendMail
 from datetime import datetime
-import logging
+import logging.config
 
-logging.basicConfig(level='DEBUG', filename='logs/app.log', filemode='a', format='%(name)s - %(levelname)s - %(asctime)s - %(message)s')
+logging.config.fileConfig('logger.ini')
+logger = logging.getLogger('consoleHandler')
 
-taskTypeList = ["daily", "weekly", "monthly", "yearly", "free"]
-daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-monthsOfTheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-                   "November", "December"]
+try:
 
-config = configparser.ConfigParser()
-config.read('tasks.ini')
+    taskTypeList = ["daily", "weekly", "monthly", "yearly", "free"]
+    daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    monthsOfTheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                       "November", "December"]
 
-now = datetime.now()
-day = now.strftime("%d")
-month = now.strftime("%m")
+    config = configparser.ConfigParser()
+    config.read('tasks.ini')
 
-
-def validate_task_type(tt):
-    if tt not in taskTypeList:
-        raise ValueError("Sorry, taskType should only be in {}".format(taskTypeList))
+    now = datetime.now()
+    day = now.strftime("%d")
+    month = now.strftime("%m")
 
 
-def validate_weekday(wd):
-    if wd not in daysOfTheWeek:
-        raise ValueError("Sorry, Weekday should only be in {}".format(daysOfTheWeek))
+    def validate_task_type(tt):
+        if tt not in taskTypeList:
+            raise ValueError("Sorry, taskType should only be in {}".format(taskTypeList))
 
 
-def validate_month(m):
-    if m not in monthsOfTheYear:
-        raise ValueError("Sorry, month should only be in  {}".format(monthsOfTheYear))
+    def validate_weekday(wd):
+        if wd not in daysOfTheWeek:
+            raise ValueError("Sorry, Weekday should only be in {}".format(daysOfTheWeek))
 
 
-def validate_day(d):
-    try:
-        d = int(d)
-    except:
-        raise ValueError("day should be an int")
-    if d < 1 or d > 31:
-        raise ValueError("Sorry, day should only be in  [1, 31]")
+    def validate_month(m):
+        if m not in monthsOfTheYear:
+            raise ValueError("Sorry, month should only be in  {}".format(monthsOfTheYear))
 
 
-def extract_type(section):
-    return config.get(section, 'Type')
+    def validate_day(d):
+        try:
+            d = int(d)
+        except:
+            raise ValueError("day should be an int")
+        if d < 1 or d > 31:
+            raise ValueError("Sorry, day should only be in  [1, 31]")
 
 
-def extract_message(section):
-    return config.get(section, 'Message')
+    def extract_type(section):
+        return config.get(section, 'Type')
 
 
-def extract_days(section):
-    return config.get(section, 'Day')
+    def extract_message(section):
+        return config.get(section, 'Message')
 
 
-def extract_months(section):
-    return config.get(section, 'Month')
+    def extract_days(section):
+        return config.get(section, 'Day')
 
 
-def extract_weekdays(section):
-    return config.get(section, 'Weekday')
+    def extract_months(section):
+        return config.get(section, 'Month')
 
 
-for i in config.sections():
+    def extract_weekdays(section):
+        return config.get(section, 'Weekday')
 
-    taskType = extract_type(i)
-    validate_task_type(taskType)
 
-    if taskType == 'daily':
-        message = extract_message(i)
-        logging.info("Everything is OK! :D I'll remember you \"{}\" every day ".format(message))
-        sendMail.sendNotification(i, message)
+    for i in config.sections():
 
-    if taskType == 'weekly':
-        for j in extract_weekdays(i).split(','):
-            validate_weekday(j)
-            if daysOfTheWeek.index(j) == now.weekday():
-                message = extract_message(i)
-                logging.info("Everything is OK! :D I'll remember you \"{}\" every day ".format(message))
-                sendMail.sendNotification(i, message)
+        taskType = extract_type(i)
+        validate_task_type(taskType)
 
-    if taskType == 'monthly':
-        for j in extract_days(i).split(','):
-            validate_day(j)
-            if day == j:
-                message = extract_message(i)
-                logging.info("Everything is OK! :D I'll remember you \"{}\" every {} of the month ".format(message, j))
-                sendMail.sendNotification(i, message)
-
-    if taskType == 'yearly':
-        m = extract_months(i)
-        d = extract_days(i)
-        if len(list(m.split(','))) > 1 or len(list(d.split(','))) > 1:
-            raise ValueError("Sorry, yearly task should only contain one specific date")
-        validate_day(d)
-        validate_month(m)
-        if d == day and monthsOfTheYear.index(m) + 1 == int(month):
+        if taskType == 'daily':
             message = extract_message(i)
-            logging.info(
-                "Everything is OK! :D I'll remember you \"{}\" every year, the {} of the month {}".format(message, day,
-                                                                                                          month))
+            logger.info("Everything is OK! :D I'll remember you \"{}\" every day ".format(message))
             sendMail.sendNotification(i, message)
 
-    if taskType == 'free':
-        for j in extract_months(i).split(','):
-            validate_month(j)
-            if monthsOfTheYear.index(j) + 1 == int(month):
-                for k in extract_days(i).split(','):
-                    validate_day(k)
-                    if day == k:
-                        message = extract_message(i)
-                        logging.info(
-                            "Everything is OK! :D I'll remember you \"{}\" every {} of the months {}".format(message,
-                                                                                                             day,
-                                                                                                             month))
-                        sendMail.sendNotification(i, message)
+        if taskType == 'weekly':
+            for j in extract_weekdays(i).split(','):
+                validate_weekday(j)
+                if daysOfTheWeek.index(j) == now.weekday():
+                    message = extract_message(i)
+                    logger.info("Everything is OK! :D I'll remember you \"{}\" every day ".format(message))
+                    sendMail.sendNotification(i, message)
+
+        if taskType == 'monthly':
+            for j in extract_days(i).split(','):
+                validate_day(j)
+                if day == j:
+                    message = extract_message(i)
+                    logger.info("Everything is OK! :D I'll remember you \"{}\" every {} of the month ".format(message, j))
+                    sendMail.sendNotification(i, message)
+
+        if taskType == 'yearly':
+            m = extract_months(i)
+            d = extract_days(i)
+            if len(list(m.split(','))) > 1 or len(list(d.split(','))) > 1:
+                raise ValueError("Sorry, yearly task should only contain one specific date")
+            validate_day(d)
+            validate_month(m)
+            if d == day and monthsOfTheYear.index(m) + 1 == int(month):
+                message = extract_message(i)
+                logger.info(
+                    "Everything is OK! :D I'll remember you \"{}\" every year, the {} of the month {}".format(message, day,
+                                                                                                              month))
+                sendMail.sendNotification(i, message)
+
+        if taskType == 'free':
+            for j in extract_months(i).split(','):
+                validate_month(j)
+                if monthsOfTheYear.index(j) + 1 == int(month):
+                    for k in extract_days(i).split(','):
+                        validate_day(k)
+                        if day == k:
+                            message = extract_message(i)
+                            logger.info(
+                                "Everything is OK! :D I'll remember you \"{}\" every {} of the months {}".format(message,
+                                                                                                                 day,
+                                                                                                                 month))
+                            sendMail.sendNotification(i, message)
+except Exception as e:
+    logger.error(e, exc_info=True)
