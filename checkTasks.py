645b31c0301f72633edcb207d2f85e1e84ed72e9
Florent BESNARD <florent.besnard@outlook.com>
2020-10-10 12:41:43 +0200

    Added weekdayofmonth

--- checkTasks.py
+++ checkTasks.py
@@ -27,6 +27,23 @@ for i in config.sections():
 			if day == j:
 				sendMail.sendNotification(i, config.get(i, 'Message'))
 
+	if taskType == 'weekdayofmonth':
+	for j in config.get(i, 'Weekday').split(','):
+		for occurrence in config.get(i, 'Occurrence').split(','):
+			if daysOfTheWeek.index(j) == now.weekday():
+				if occurrence.lower() == "first" and 1 <= int(day) <= 7:
+					sendMail.sendNotification(i, config.get(i, 'Message'))
+				elif occurrence.lower() == "second" and 8 <= int(day) <= 14:
+					sendMail.sendNotification(i, config.get(i, 'Message'))
+				elif occurrence.lower() == "third" and 15 <= int(day) <= 21:
+					sendMail.sendNotification(i, config.get(i, 'Message'))
+				elif occurrence.lower() == "fourth" and 22 <= int(day) <= 28:
+					sendMail.sendNotification(i, config.get(i, 'Message'))
+				elif occurrence.lower() == "last" and 25 <= int(day) <= 31:
+					sendMail.sendNotification(i, config.get(i, 'Message'))
+				else:
+					continue
+
 	if taskType == 'yearly':
 		if config.get(i, 'Day') == day and monthsOfTheYear.index(config.get(i, 'Month')) + 1 == int(month):
 			sendMail.sendNotification(i, config.get(i, 'Message'))
